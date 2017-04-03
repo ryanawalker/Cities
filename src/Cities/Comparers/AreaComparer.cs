8773c7412ae6952930a5eff7ed3dd944d472bd00
Ryan A. Walker <rawn8c@mail.umsl.edu>
2017-04-03 18:49:15 -0500

    Adds comparers for population, state, and land area.

---
+++ src/Cities/Comparers/AreaComparer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Cities.Comparers
+{
+    public class AreaComparer : IComparer<City>
+    {
+        public int Compare(City x, City y)
+        {
+            if (x.Area < y.Area)
+            {
+                return 1;
+            }
+            else if(x.Area > y.Area)
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
